fcc049b4f31c71efb80833c850114b035ac4f97b
GenioFalco <zhenechka.sokolov.02@mail.ru>
2025-02-06 00:36:46 +0400

    оптимизация

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,3 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import ProfileView from '@/views/ProfileView.vue'
