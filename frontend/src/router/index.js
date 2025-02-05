d87a3e798c4321be603c4f9a61e92149b777368d
GenioFalco <zhenechka.sokolov.02@mail.ru>
2025-02-06 00:32:11 +0400

    оптимизация

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import TasksView from '@/views/TasksView.vue'
 import PhotoAlbumView from '@/views/PhotoAlbumView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
